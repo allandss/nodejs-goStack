d043b4fdea4a2b3c03d5381c19f2ffd0340d0ba0
Allan Santos <allandssilva@gmail.com>
2019-10-27 15:50:45 -0300

    Criando migration de usuário

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'gibarber',
+  database: 'gobarber',
   define: {
     timestamps: true,
     underscored: true,
