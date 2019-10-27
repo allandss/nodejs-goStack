947ca8789fe51c43ff5c8d1aa3c70be96d2c540a
Allan Santos <allandssilva@gmail.com>
2019-10-27 16:34:24 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Allan' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Allan Santos',
+    email: 'allandssilva@gmail.com',
+    password_hash: '1234567890',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
